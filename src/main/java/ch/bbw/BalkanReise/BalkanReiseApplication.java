9c80c5e94a284487820eae8d134f18a5daac7569
aboroja12 <andrej.boroja@gmail.com>
2022-06-02 16:47:15 +0200

    first commit

---
+++ src/main/java/ch/bbw/BalkanReise/BalkanReiseApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.BalkanReise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalkanReiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalkanReiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/BalkanReise/BalkanReiseApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.BalkanReise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalkanReiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
